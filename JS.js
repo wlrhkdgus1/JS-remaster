47587c99d8131741514b4cd21c91766c757c5048
Ji Gwang Hyun <gwgw4578@gmail.com>
2021-02-06 17:15:56 +0900

    js remaster start

---
+++ JS.js
@@ -0,0 +1,7 @@
+//var newContent = prompt("JavaScript는 HTML 페이지를 변경할 수 있습니다.");
+//document.body.innerText = newContent;
+var a = document.getElementById("level1");
+a.innerHTML= "<h1>hello</h1>"
+var b = document.getElementById("logo");
+b.setAttribute("src","1.png")
+
